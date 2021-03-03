cdae918c8e61e07d8eecb5ea86c92accbfb7649e
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-03-03 10:03:47 +0530

    feat : added sort by tag

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,12 +1,56 @@
 import { NoteCard } from "./NoteCard";
 import "./NotesContainer.css";
 
-function NotesContainer({ notes }) {
+function NotesContainer({ notes, selectedTag }) {
+	const filteredArr = notes.filter((item) => {
+		if (selectedTag === "all") {
+			return true;
+		} else if (
+			selectedTag === item.tag.find((item) => item === selectedTag)
+		) {
+			return true;
+		} else return false;
+	});
+	const pinnedNotes = filteredArr.filter((item) => item.pinned);
+	const otherNotes = filteredArr.filter((item) =>
+		item.pinned ? false : true
+	);
+	console.log(pinnedNotes.length);
 	return (
 		<div className="notes-container">
-			{notes.map((item) => (
+			{pinnedNotes.length >= 1 && (
+				<div className="pinned-notes">
+					<h5>PINNED</h5>
+					<div className="pinned-notes-container">
+						{pinnedNotes.map((item) => (
+							<NoteCard
+								key={item.id}
+								notes={notes}
+								note={item}
+								// setNotes={setNotes}
+							/>
+						))}
+					</div>
+				</div>
+			)}
+			<div className="other-notes">
+				{pinnedNotes.length > 0 && otherNotes.length >= 1 && (
+					<h5>OTHER</h5>
+				)}
+				<div className="other-notes-container">
+					{otherNotes.map((item) => (
+						<NoteCard
+							key={item.id}
+							notes={notes}
+							note={item}
+							// setNotes={setNotes}
+						/>
+					))}
+				</div>
+				{/* {notes.map((item) => (
 				<NoteCard key={item.id} note={item} />
-			))}
+			))} */}
+			</div>
 		</div>
 	);
 }
