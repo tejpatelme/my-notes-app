eacaff798e937cfebceeb4f15f7c96981afd4f1b
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-03-03 10:04:17 +0530

    Rendered TagList

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 import { AddNote } from "./components/AddNote";
 import { v4 as uuidv4 } from "uuid";
 import { NotesContainer } from "./components/NotesContainer";
+import { TagList } from "./components/TagList";
 
 function App() {
 	const [notes, setNotes] = useState([
 		{
 			id: uuidv4(),
-			title: "Hello4",
+			title: "Hello",
 			text: "Add a note to get started",
 			tag: ["all"],
 			color: "white",
@@ -16,10 +17,22 @@ function App() {
 		},
 	]);
 
+	const [tags, setTags] = useState(["personal", "work", "journal", "all"]);
+
+	const [selectedTag, setSelectedTag] = useState("all");
+
+	console.log(selectedTag);
+
 	return (
 		<div className="App">
-			<AddNote notes={notes} setNotes={setNotes} />
-			<NotesContainer notes={notes} />
+			<AddNote
+				notes={notes}
+				setNotes={setNotes}
+				tags={tags}
+				setTags={setTags}
+			/>
+			<TagList tags={tags} setSelectedTag={setSelectedTag} />
+			<NotesContainer notes={notes} selectedTag={selectedTag} />
 		</div>
 	);
 }
