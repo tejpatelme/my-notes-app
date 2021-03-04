61b13a458a387f9219b5fbc82e4ec9e296103869
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-03-04 18:19:37 +0530

    added re-export file for cleaner imports

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { AddNote } from "./AddNote";
+export { EditModal } from "./EditModal";
+export { EditNote } from "./EditNote";
+export { NoteCard } from "./NoteCard";
+export { NotesContainer } from "./NotesContainer";
+export { TagChip } from "./TagChip";
+export { TagList } from "./TagList";
