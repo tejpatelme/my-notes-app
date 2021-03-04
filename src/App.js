1f40d945a7ce6b561bd486a036a63b586a19ab49
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-03-04 18:17:38 +0530

    lifted state of note up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import { useState } from "react";
-import { AddNote } from "./components/AddNote";
 import { v4 as uuidv4 } from "uuid";
-import { NotesContainer } from "./components/NotesContainer";
-import { TagList } from "./components/TagList";
+import { EditModal, AddNote, NotesContainer, TagList } from "./components";
 
 function App() {
+	const [note, setNote] = useState({
+		title: "",
+		text: "",
+		tag: ["all"],
+		color: "white",
+		pinned: false,
+	});
+
 	const [notes, setNotes] = useState([
 		{
 			id: uuidv4(),
@@ -21,6 +27,34 @@ function App() {
 
 	const [selectedTag, setSelectedTag] = useState("all");
 
+	const [showModal, setShowModal] = useState(false);
+
+	const [editNoteObj, setEditNoteObj] = useState({
+		id: "",
+		title: "",
+		text: "",
+		tag: ["all"],
+		color: "white",
+		pinned: false,
+	});
+
+	const editNote = (noteid) => {
+		notes.forEach((item) => {
+			if (item.id === noteid) {
+				setEditNoteObj({
+					id: item.id,
+					title: item.title,
+					text: item.text,
+					tag: item.tag,
+					color: item.color,
+					pinned: item.pinned,
+				});
+			}
+		});
+	};
+
+	console.log(editNoteObj);
+
 	console.log(selectedTag);
 
 	return (
@@ -30,9 +64,29 @@ function App() {
 				setNotes={setNotes}
 				tags={tags}
 				setTags={setTags}
+				note={note}
+				setNote={setNote}
 			/>
 			<TagList tags={tags} setSelectedTag={setSelectedTag} />
-			<NotesContainer notes={notes} selectedTag={selectedTag} />
+			<NotesContainer
+				notes={notes}
+				selectedTag={selectedTag}
+				setNotes={setNotes}
+				showModal={showModal}
+				setShowModal={setShowModal}
+				editNote={editNote}
+			/>
+			{showModal && (
+				<EditModal
+					notes={notes}
+					setNotes={setNotes}
+					tags={tags}
+					setTags={setTags}
+					note={editNoteObj}
+					setNote={setEditNoteObj}
+					setShowModal={setShowModal}
+				/>
+			)}
 		</div>
 	);
 }
