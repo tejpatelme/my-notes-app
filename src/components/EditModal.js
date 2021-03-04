e7256d573df6c98250e71b6b572fa9df96106203
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-03-04 18:18:31 +0530

    create : modal to edit notes

---
+++ src/components/EditModal.js
@@ -0,0 +1,28 @@
+import "./EditModal.css";
+import { EditNote } from "./EditNote";
+
+function EditModal({
+	note,
+	notes,
+	tags,
+	setTags,
+	setNote,
+	setNotes,
+	setShowModal,
+}) {
+	return (
+		<div className="edit-modal">
+			<EditNote
+				notes={notes}
+				setNotes={setNotes}
+				tags={tags}
+				setTags={setTags}
+				note={note}
+				setNote={setNote}
+				setShowModal={setShowModal}
+			/>
+		</div>
+	);
+}
+
+export { EditModal };
