ee0698ed24c4d82e76c2bf319afd5f718c3a42ad
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-03-04 14:16:46 +0530

    add : pin and remove button

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,11 +1,32 @@
 import "./NoteCard.css";
 import "./AddNote.css";
+import pin from "../assets/icons/push_pin-24px.svg";
+import pinfilled from "../assets/icons/pinfilled.svg";
+import remove from "../assets/icons/delete-24px.svg";
 import { TagChip } from "./TagChip";
 
-function NoteCard({ note }) {
+function NoteCard({ note, notes, setNotes }) {
+	const changePinned = () => {
+		setNotes(
+			notes.map((item) =>
+				item.id === note.id ? { ...item, pinned: !item.pinned } : item
+			)
+		);
+	};
+
+	const removeNote = () => {
+		setNotes(notes.filter((item) => (item.id === note.id ? false : true)));
+	};
 	return (
 		<div className={`note-card ${note.color}`}>
 			<div className="title">{note.title}</div>
+			<button className="pin-button" onClick={changePinned}>
+				{note.pinned ? (
+					<img src={pinfilled} alt="pin" />
+				) : (
+					<img src={pin} alt="pin" />
+				)}
+			</button>
 			<div className="text">{note.text}</div>
 			<div className="tags">
 				{note.tag.map((item) => {
@@ -14,6 +35,9 @@ function NoteCard({ note }) {
 					else return null;
 				})}
 			</div>
+			<button onClick={removeNote} className="remove-button">
+				<img src={remove} alt="remove" />
+			</button>
 		</div>
 	);
 }
